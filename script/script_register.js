348d5ab2c52c012d63a0a0f65d01a85f5cec4cf9
nourrane <97787753+nourrane@users.noreply.github.com>
2022-03-08 07:54:16 +0100

    Add files via upload

--- script/script_register.js
+++ script/script_register.js
@@ -100,3 +100,30 @@ form.addEventListener("submit", function (event) {
 function show_value(x){
     document.getElementById('budget').innerHTML = x + "€";
 }
+
+//password
+function show() {
+    var p = document.getElementById('password');
+    p.setAttribute('type', 'text');
+}
+//password
+function hide() {
+    var p = document.getElementById('password');
+    p.setAttribute('type', 'password');
+}
+
+var pwShown = 0;
+
+document.getElementById("eye").addEventListener("click", function () {
+    if (pwShown == 0) {
+        pwShown = 1;	
+				document.getElementById("eye").classList.add("fa-eye-slash");
+				document.getElementById("eye").classList.remove("fa-eye");
+        show();
+    } else {
+        pwShown = 0;
+			  document.getElementById("eye").classList.add("fa-eye");
+				document.getElementById("eye").classList.remove("fa-eye-slash");
+        hide();
+    }
+}, false);
